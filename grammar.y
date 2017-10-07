f05221e711d331a5f2897e3aeb2af723c7413e7e
joner1992 <filipejoner@gmail.com>
2017-10-07 15:10:24 -0300

    Rules added to grammar, input from Johann, main and scanner.l changed

--- grammar.y
+++ grammar.y
@@ -1,3 +1,20 @@
+/*
+    Etapa 2 Trabalho de Compiladores:
+        Filipe Joner e Vinicius Pittigliani
+*/
+
+%{
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include "hash.h"
+	
+	int yylex();
+	int yyerror(char *message);
+	extern int getLineNumber();
+
+%}
+
 %token KW_BYTE
 %token KW_SHORT
 %token KW_LONG
@@ -27,13 +44,12 @@
 
 %token TOKEN_ERROR
 
+%left '<' '>'  
+%left '!' OPERATOR_LE OPERATOR_GE OPERATOR_EQ OPERATOR_NE OPERATOR_AND OPERATOR_OR
 %left '+' '-'
 %left '*' '/'
-%{
- void yyerror(char *);
- int yylex(void);
- int sym[26];
-%}
+
+%start prog
 
 %%
 
@@ -45,24 +61,45 @@ dec : expr
     | vardec
     ;
 
-vardec : TK_IDENTIFIER':' type '=' value
+vardec : TK_IDENTIFIER ':' type '=' value ';'
+    | TK_IDENTIFIER ':' type '['LIT_INTEGER']' init';'
     ;
+
 value: LIT_INTEGER
     | LIT_REAL
     | LIT_CHAR
-    | LIT_STRING
+    ;
 
-fundec : type TK_IDENTIFIER '(' listArgs ')' cmd
-    | type TK_IDENTIFIER '('  ')' cmd
+init : value init
+    |
+    ;
+
+fundec : '(' type ')' TK_IDENTIFIER '(' listArgs ')' block
+    ;
+
+block : '{' listCmd '}'
+    ;
+
+listCmd : cmd ';' listCmd
+    | cmd
     ;
 
 cmd : TK_IDENTIFIER '=' expr
+    | TK_IDENTIFIER '[' expr ']' '=' expr
+    | KW_READ '>' TK_IDENTIFIER
+    | KW_PRINT listPrint
+    | KW_RETURN expr
+    | KW_IF '(' expr ')' KW_THEN cmd
+    | KW_IF '(' expr ')' KW_THEN cmd KW_ELSE cmd
+    | KW_WHILE '(' expr ')' cmd
 	| block
+    |
 	;
 
-block : '{' listCmd '}'
+listPrint : printable ',' listPrint
+    ;
 
-listCmd : cmd listCmd
+printable : expr
     |
     ;
 
@@ -70,13 +107,11 @@ type : KW_BYTE
     | KW_SHORT
     | KW_LONG
     | KW_FLOAT
+    | KW_DOUBLE
     ;
 
-/* inicio nosso */
 
-expr :
-    KW_SHORT { $$ = $1; }
-    | expr '+' expr { $$ = $1 + $3; }
+expr : expr '+' expr { $$ = $1 + $3; }
     | expr '-' expr { $$ = $1 - $3; }
     | expr '*' expr { $$ = $1 * $3; }
     | expr '/' expr { $$ = $1 / $3; }
@@ -93,16 +128,19 @@ expr :
     | TK_IDENTIFIER '(' listArgs  ')'
     | TK_IDENTIFIER '[' expr ']'
     | TK_IDENTIFIER
-    | LIT_INTEGER   { $$ = $1; }
-    | LIT_REAL  { $$ = $1; }
+    | value
+    | LIT_STRING
     ;
 
 listArgs : arg ',' listArgs
-    | arg
+    |
     ;
 
 arg: TK_IDENTIFIER ':' type
-  ;
+    | TK_IDENTIFIER
+    | value
+    | LIT_STRING
+    ;
 
 %%
 
@@ -110,7 +148,4 @@ void yyerror(char *s) {
  fprintf(stderr, "%s\n", s);
  return 0;
 }
-int main(void) {
- yyparse();
- return 0;
-}
+
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include "y.tab.h"
-#include "hash.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,12 +5,15 @@
 * Filipe Joner
 * Vinícius Pittigliani Perego
 */
-int yylex();
+extern int yylex();
 extern char *yytext;
 extern int lines;
 extern FILE *yyin;
 extern HASH_NODE * table[HASH_SIZE];
 extern int getLineNumber(void);
+extern int yyparse();
+extern void initMe();
+extern void hashPrint();
 
 int isRunning();
 
@@ -32,48 +33,9 @@ int main(int argc, char *argv[]) {
 	initMe();
 	printf("Hash init complete.\n");
 	while(isRunning()) {
-		token = yylex();
-
-		switch(token) {
-
-			case KW_IF: {fprintf(stderr,"%d. Found If Token: %d\n", lines, KW_IF); break;}
-			case KW_BYTE:	{fprintf(stderr,"%d. Found Byte Token: %d\n", lines, KW_BYTE); break;}
-			case KW_SHORT:	{fprintf(stderr,"%d. Found Short Token: %d\n", lines, KW_SHORT); break;}
-			case KW_LONG:	{fprintf(stderr,"%d. Found Long Token: %d\n", lines, KW_LONG); break;}
-			case KW_FLOAT:	{fprintf(stderr,"%d. Found Float Token: %d\n", lines, KW_FLOAT); break;}
-			case KW_DOUBLE:	{fprintf(stderr,"%d. Found Double Token: %d\n", lines, KW_DOUBLE); break;}
-			case KW_THEN:	{fprintf(stderr,"%d. Found Then Token: %d\n", lines, KW_THEN); break;}
-			case KW_ELSE:	{fprintf(stderr,"%d. Found Else Token: %d\n", lines, KW_ELSE); break;}
-			case KW_WHILE:	{fprintf(stderr,"%d. Found While Token: %d\n", lines, KW_WHILE); break;}
-			case KW_FOR:	{fprintf(stderr,"%d. Found For Token: %d\n", lines, KW_FOR); break;}
-			case KW_READ:	{fprintf(stderr,"%d. Found Read Token: %d\n", lines, KW_READ); break;}
-			case KW_PRINT:	{fprintf(stderr,"%d. Found Print Token: %d\n", lines, KW_PRINT); break;}
-			case KW_RETURN:	{fprintf(stderr,"%d. Found Return Token: %d\n", lines, KW_PRINT); break;}
-
-			case OPERATOR_LE:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_LE); break;}
-			case OPERATOR_GE:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_GE); break;}
-			case OPERATOR_EQ:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_EQ); break;}
-			case OPERATOR_NE:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_NE); break;}
-			case OPERATOR_AND:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_AND); break;}
-			case OPERATOR_OR:	{fprintf(stderr,"%d. Found Operator Token: %d\n", lines, OPERATOR_OR); break;}
-
-			case LIT_INTEGER: {fprintf(stderr,"%d. Found Literal Token: %d\n", lines, LIT_INTEGER); break;}
-			case LIT_REAL:  {fprintf(stderr,"%d. Found Literal Token: %d\n", lines, LIT_REAL); break;}
-
-			case LIT_CHAR: {fprintf(stderr,"%d. Found Literal Token: %d\n", lines, LIT_CHAR); break;}
-			case LIT_STRING: {fprintf(stderr,"%d. Found Literal Token: %d\n", lines, LIT_STRING); break;}
-
-			case TK_IDENTIFIER: {fprintf(stderr,"%d. Found Identifier Token: %d (with value ""%s"")\n", lines, TK_IDENTIFIER,yytext); break;}
-
-			case TOKEN_ERROR: {fprintf(stderr,"%d. Found Error Token: %d\n", lines, TOKEN_ERROR); break;}
-
-			default: {fprintf(stderr,"%d. Found Operator Token:%c (ascii value %d)\n", lines,(char)token,token); break;}
-		}
-		if(token!=TOKEN_ERROR)
-			hashInsert(token,yytext);
-
+		yyparse();
 	}
 	hashPrint();
-	printf("End. Total input lines:%d\n",getLineNumber() );
+	printf("Congratulations, Syntax is correct!s");
     return 0;
 }
