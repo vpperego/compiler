97541f8dfece1dd0161b5ec2c1e9f2bb6273962e
vpperego <vinicius.pittigliani@gmail.com>
2017-10-08 11:54:23 -0300

    Makefile fix++

---
+++ y.tab.h
@@ -0,0 +1,114 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    KW_BYTE = 258,
+    KW_SHORT = 259,
+    KW_LONG = 260,
+    KW_FLOAT = 261,
+    KW_DOUBLE = 262,
+    KW_IF = 263,
+    KW_THEN = 264,
+    KW_ELSE = 265,
+    KW_WHILE = 266,
+    KW_FOR = 267,
+    KW_READ = 268,
+    KW_RETURN = 269,
+    KW_PRINT = 270,
+    OPERATOR_LE = 271,
+    OPERATOR_GE = 272,
+    OPERATOR_EQ = 273,
+    OPERATOR_NE = 274,
+    OPERATOR_AND = 275,
+    OPERATOR_OR = 276,
+    TK_IDENTIFIER = 277,
+    LIT_INTEGER = 278,
+    LIT_REAL = 279,
+    LIT_CHAR = 280,
+    LIT_STRING = 281,
+    TOKEN_ERROR = 282
+  };
+#endif
+/* Tokens.  */
+#define KW_BYTE 258
+#define KW_SHORT 259
+#define KW_LONG 260
+#define KW_FLOAT 261
+#define KW_DOUBLE 262
+#define KW_IF 263
+#define KW_THEN 264
+#define KW_ELSE 265
+#define KW_WHILE 266
+#define KW_FOR 267
+#define KW_READ 268
+#define KW_RETURN 269
+#define KW_PRINT 270
+#define OPERATOR_LE 271
+#define OPERATOR_GE 272
+#define OPERATOR_EQ 273
+#define OPERATOR_NE 274
+#define OPERATOR_AND 275
+#define OPERATOR_OR 276
+#define TK_IDENTIFIER 277
+#define LIT_INTEGER 278
+#define LIT_REAL 279
+#define LIT_CHAR 280
+#define LIT_STRING 281
+#define TOKEN_ERROR 282
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
